1282da328ba1d88e6dce41d7441dd8ce6691ef98
sivakrishnareddy826 <sivakrishnareddy826@gmail.com>
2024-12-29 15:57:51 +0530

    Removed unused sortings

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EmployeeCrudOperations
@@ -16,7 +9,7 @@ namespace EmployeeCrudOperations
         public Form1()
         {
             InitializeComponent();
-            form2=new Form2(this);
+            form2 = new Form2(this);
         }
         public void display()
         {
@@ -26,7 +19,7 @@ namespace EmployeeCrudOperations
         private void button1_Click(object sender, EventArgs e)
         {
 
-           form2.clear();
+            form2.clear();
             form2.saveInfo();
             form2.ShowDialog();
         }
@@ -43,28 +36,28 @@ namespace EmployeeCrudOperations
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            dbStudent.displayAndSearch("SELECT ID,Name,Reg,Class,Section FROM student1 WHERE Name LIKE'%"+ txtSearch.Text+"%'", dataGridView1);
+            dbStudent.displayAndSearch("SELECT ID,Name,Reg,Class,Section FROM student1 WHERE Name LIKE'%" + txtSearch.Text + "%'", dataGridView1);
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             //Edit
-            if(e.ColumnIndex == 0)
+            if (e.ColumnIndex == 0)
             {
                 form2.clear();
                 form2.id = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
                 form2.name = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
                 form2.reg = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
                 form2.@Class = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
-                 form2.Section = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+                form2.Section = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
                 form2.updateInfo();
                 form2.ShowDialog();
                 return;
             }
             //delete
-            if(e.ColumnIndex == 1)
+            if (e.ColumnIndex == 1)
             {
-               if (MessageBox.Show("Are You Want to Delete Student Records?","Information",MessageBoxButtons.YesNoCancel,MessageBoxIcon.Information)==DialogResult.Yes)
+                if (MessageBox.Show("Are You Want to Delete Student Records?", "Information", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information) == DialogResult.Yes)
                 {
                     dbStudent.deleteStudent(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
                     display();
